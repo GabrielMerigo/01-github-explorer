64195c2f9d4e70ad517c35749a2e4ba2a72f54b2
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-10 12:51:11 -0300

    mais dependencias

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  }, 
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }) 
+  ], 
   module: {
     rules: [
       {
