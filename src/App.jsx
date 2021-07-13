b1ccd79b95bfda86017bed09acfc5bc00aa6a201
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-12 21:44:13 -0300

    incinado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './styles/global.scss';
 import { RepositoryList } from './components/RepositoryList';
-import { Counter } from './components/Counter';
 
 export default function App() {
   return (
     <>
       <RepositoryList />
-      <Counter />
     </>
   )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useState } from "react";
+import '../styles/repositories.scss'
 
 const repository = {
   name: 'unform',
@@ -6,15 +8,20 @@ const repository = {
   link:"https://github.com/unform"
 }
 
-const usuarios = ['gabriel', 'diego', 'jorge']
-usuarios.push('rafacamarda')
+//https://api.github.com/users/gabrielmerigo/repos
 
 export function RepositoryList() {
+  const [repos, setRepos] = useState([])
+
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
       </ul>
     </section>
